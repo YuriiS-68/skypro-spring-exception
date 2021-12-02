a67698b4419ae75743b0d9bf85221dba6f6444bc
Yuvis <yurii_s@bigmir.net>
2021-12-02 11:07:14 +0200

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/skyprospringexception/SkyproSpringExceptionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.skyprospringexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproSpringExceptionApplication.class, args);
+    }
+
+}
