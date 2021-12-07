b75343af04b34140d249c78b718809e3a204ab99
Yuvis <yurii_s@bigmir.net>
2021-12-08 00:40:17 +0200

    homework to lesson Collection

--- src/main/java/pro/sky/java/course2/skyprospringexception/service/impl/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/java/course2/skyprospringexception/service/impl/EmployeeServiceImpl.java
@@ -1,45 +1,42 @@
-package pro.sky.java.course2.skyprospringexception.service;
+package pro.sky.java.course2.skyprospringexception.service.impl;
 
 import org.springframework.stereotype.Service;
-import pro.sky.java.course2.skyprospringexception.exception.ArrayIsFullException;
 import pro.sky.java.course2.skyprospringexception.exception.NoExistEmployeeException;
 import pro.sky.java.course2.skyprospringexception.model.Employee;
+import pro.sky.java.course2.skyprospringexception.service.EmployeeService;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
-    private final Employee[] employees = new Employee[3];
+public class EmployeeServiceImpl implements EmployeeService {
+    private final List<Employee> employees;
 
-    @Override
-    public String addEmployee(String firstName, String lastName) {
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] == null){
-                employees[i] = createEmployee(firstName, lastName);
-                break;
-            }
-            if (employees[i] != null && i == employees.length - 1){
-                throw new ArrayIsFullException();
-            }
-        }
-        return Arrays.toString(employees);
+    public EmployeeServiceImpl() {
+        employees = new ArrayList<>();
     }
 
     @Override
-    public String delEmployee(String firstName, String lastName) {
+    public Employee addEmployee(String firstName, String lastName) {
+        Employee newEmployee = createEmployee(firstName, lastName);
+        employees.add(newEmployee);
+        return newEmployee;
+    }
+
+    @Override
+    public void delEmployee(String firstName, String lastName) {
         boolean isExist = false;
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i] != null
-                    && employees[i].getFirstName().equals(firstName)
-                    && employees[i].getLastName().equals(lastName)){
-                employees[i] = null;
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i) != null
+                    && employees.get(i).getFirstName().equals(firstName)
+                    && employees.get(i).getLastName().equals(lastName)){
+                employees.remove(employees.get(i));
                 isExist = true;
             }
         }
         if (!isExist){
             throw new NoExistEmployeeException();
         }
-        return Arrays.toString(employees);
     }
 
     @Override
@@ -54,6 +51,11 @@ public class EmployeeServiceImpl implements EmployeeService{
             throw new NoExistEmployeeException();
         }
         return foundEmployee;
+    }
+
+    @Override
+    public List<Employee> getEmployeesList() {
+        return employees;
     }
 
     private Employee createEmployee(String firstName, String lastName){
